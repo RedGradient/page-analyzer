2bce7387d941b4c5dd2fa5939c4c7273190932c3
redgradient <neutral1shade@yahoo.com>
2023-01-14 18:08:11 +0400

    refactor

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -1,12 +1,19 @@
 package hexlet.code.controllers;
 
 import hexlet.code.domain.Url;
+import hexlet.code.domain.UrlCheck;
 import hexlet.code.domain.query.QUrl;
+import hexlet.code.domain.query.QUrlCheck;
 import io.javalin.http.Handler;
 import io.javalin.http.HttpCode;
 
-import static hexlet.code.App.getLogger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
+import static hexlet.code.App.getLogger;
 
 public class UrlController {
     public static Handler mainPage = ctx -> {
@@ -16,56 +23,106 @@ public class UrlController {
     public static Handler getUrls = ctx -> {
         var urls = new QUrl().findList();
 
+        HashMap<String, Map<String, String>> urlsAndChecks = new HashMap<>();
+        for (var url : urls) {
+            var urlCheck = new QUrlCheck()
+                    .url.equalTo(url)
+                    .createdAt.desc()
+                    .setMaxRows(1)
+                    .findOneOrEmpty();
+            String statusCode = "";
+            String latestCheck = "";
+            if (urlCheck.isPresent()) {
+                statusCode = String.valueOf(urlCheck.get().getStatusCode());
+                latestCheck = urlCheck.get().getCreatedAt().toString();
+            }
+            urlsAndChecks.put(
+                    url.getName(),
+                    Map.of("statusCode", statusCode, "latestCheck", latestCheck)
+            );
+        }
+
         ctx.attribute("urls", urls);
+        ctx.attribute("urlsAndChecks", urlsAndChecks);
+
         ctx.render("list.html");
     };
 
     public static Handler postUrls = ctx -> {
-        String urlString = ctx.formParam("url");
-
-        if (urlString == null) {
-            getLogger().info("Url is null. Redirect to '/'");
+        var urlString = ctx.formParamAsClass("url", String.class).get();
+        URL url = null;
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException e) {
             ctx.redirect("/");
             return;
         }
 
-        var url = new java.net.URL(urlString);
         var parsedUrl = url.getProtocol() + "://" + url.getAuthority();
-
-        try {
+        var count = new QUrl().name.equalTo(parsedUrl).findCount();
+        if (count > 0) {
+            ctx.sessionAttribute("flash", "Страница уже существует");
+            ctx.sessionAttribute("alertType", "alert-warning");
+        } else {
             new Url(parsedUrl).save();
             getLogger().info("Added: " + parsedUrl);
+
             ctx.sessionAttribute("flash", "Страница успешно добавлена");
-        } catch (Exception e) {
-            getLogger().warn(e.getMessage());
-            var message = String.format("URL %s already exists. Nothing changed", parsedUrl);
-            getLogger().info(message);
-            ctx.sessionAttribute("flash", "Страница уже существует");
+            ctx.sessionAttribute("alertType", "alert-primary");
         }
 
         ctx.redirect("/urls");
     };
 
     public static Handler showUrl = ctx -> {
-
-        var strId = ctx.pathParam("id");
-        Integer id;
-
         try {
-            id = Integer.valueOf(strId);
-        } catch (NumberFormatException e) {
+            var id = Integer.valueOf(ctx.pathParam("id"));
+            var url = new QUrl().id.equalTo(id).findOneOrEmpty().orElseThrow();
+            var urlChecks = new QUrlCheck()
+                    .id.desc()
+                    .url.equalTo(url)
+                    .findList();
+            ctx.attribute("url", url);
+            ctx.attribute("urlChecks", urlChecks);
+            ctx.render("show.html");
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
             ctx.status(HttpCode.NOT_FOUND);
-            return;
-        }
-
-        var url = new QUrl().id.equalTo(id).findOneOrEmpty();
-        if (url.isEmpty()) {
-            getLogger().info(String.format("Url with id=%s not found", id));
+        } catch (Exception e) {
             ctx.status(HttpCode.NOT_FOUND);
-            return;
         }
+    };
 
-        ctx.attribute("url", url.get());
-        ctx.render("show.html");
+    public static Handler checkUrl = ctx -> {
+        try {
+            var id = Integer.valueOf(ctx.pathParam("id"));
+            var url = new QUrl().id.equalTo(id).findOneOrEmpty().orElseThrow();
+
+
+            // --- check url ---
+
+            new UrlCheck(
+                    200,
+                    "Example Title",
+                    "Some Text",
+                    "h1-h1-h1-h1-h1-h1",
+                    url
+            ).save();
+            // -----------------
+
+
+            var urlChecks = new QUrlCheck()
+                    .url.equalTo(url)
+                    .id.desc()
+                    .findList();
+            ctx.sessionAttribute("urlChecks", urlChecks);
+            ctx.sessionAttribute("flash", "Страница успешно проверена");
+            ctx.sessionAttribute("alertType", "alert-primary");
+            ctx.redirect("/urls/" + id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            ctx.sessionAttribute("flash", "Что-то пошло не так");
+            ctx.sessionAttribute("alertType", "alert-warning");
+        }
     };
 }
