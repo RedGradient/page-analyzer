27cd60f7d2a7416aa9e20a48c835552c03ea2edd
redgradient <neutral1shade@yahoo.com>
2023-01-23 16:54:02 +0400

    add: seed.sql

---
+++ app/src/test/resources/dbscripts/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO url (name, created_at) VALUES
+    ('https://google.com', '2023-01-23'),
+    ('https://apple.com', '2023-01-23'),
+    ('https://facebook.com', '2023-01-23'),
+    ('https://my-site.com:8080', '2023-01-23');
